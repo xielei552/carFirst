75ea405e266074bca91bd20c7161d98bf63317c7
Lei Xie <xielei552@gmail.com>
2017-04-04 22:22:12 -0400

    + add dev vue.js, first hello Vue! message

--- index.php
+++ index.php
@@ -18,13 +18,14 @@
     <![endif]-->
   </head>
   <body>
-    <h1>Hello, world!</h1>
-
+    <div id="app">
+      {{ message }}
+    </div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-    <!-- development vue.js -->
-    <script src="js/vue.js"></script>
+    <script type="text/javascript" src="js/vue.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
   </body>
 </html>
